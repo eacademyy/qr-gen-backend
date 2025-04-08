93daf655c7ad373873209fb30c4f4ace768d0152
eacademyy <eacademyy@gmail.com>
2025-04-08 16:48:35 +0530

    Qr Project Seventh Commit

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
+console.log("MONGODB_URI:", process.env.DB_URL); // debug log
+
+
 mongoose.connect(process.env.DB_URL)
 
 mongoose.connection.on("connected",()=>{
