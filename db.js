46dff034ea61fec9cf3a5eca8bf560fcb6784743
eacademyy <eacademyy@gmail.com>
2025-04-08 16:55:16 +0530

    Qr Project Eigth Commit

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-console.log("MONGODB_URI:", process.env.DB_URL); // debug log
+// console.log("MONGODB_URI:", process.env.DB_URL); // debug log
 
 
 mongoose.connect(process.env.DB_URL)
