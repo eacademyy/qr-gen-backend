76c0bff60028d4ffa8877308fc94764f32678f8d
eacademyy <eacademyy@gmail.com>
2025-04-08 17:10:23 +0530

    Qr Project Ninth Commit

---
+++ app.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const db = require('./db')
+
+// const userModel = require('./Model/User')
+
+// all router will be here
+const userRouter = require('./Route/UserRoute')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors({
+    origin:'http://localhost:5173'
+}))
+
+// All route initiate here
+app.use('/userapi',userRouter)
+
+const port = process.env.PORT
+
+
+
+app.listen(port,(req,res)=>{
+    console.log(`Server is running on : http://localhost:${port}`)
+})
